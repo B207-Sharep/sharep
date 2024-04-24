0488d31e74591866edefc3d7af9daa6efd60468a
jack <yjk9805@naver.com>
2024-04-24 12:56:56 +0900

    [Feat] Account 제약조건 설정

--- be/src/main/java/com/sharep/be/modules/account/Account.java
+++ be/src/main/java/com/sharep/be/modules/account/Account.java
@@ -1,9 +1,7 @@
 package com.sharep.be.modules.account;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
@@ -16,12 +14,22 @@ public class Account {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-
+    @Column(length = 8, nullable = false)
     String nickname;
 
+    @Column(nullable = false)
     String password;
 
+    @Column(nullable = false)
     String email;
 
     LocalDateTime createdAt;
+
+    @Builder
+    public Account(String nickname, String password, String email) {
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+        this.createdAt = LocalDateTime.now();
+    }
 }
