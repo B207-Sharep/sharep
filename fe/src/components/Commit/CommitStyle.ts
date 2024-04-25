0883b058dc7bc00db405d183b473130c9049ea3e
seojeong <sjytis14@naver.com>
2024-04-26 01:27:17 +0900

    [Feat] commit 컴포넌트 닫힌 accordian 구현

--- fe/src/components/Commit/CommitStyle.ts
+++ fe/src/components/Commit/CommitStyle.ts
@@ -1,21 +1,45 @@
+import { PALETTE } from '@/styles';
 import styled from 'styled-components';
-import { PALETTE } from '../../styles/globals';
 
-export const CommitBox = styled.div`
-  width: 100px;
+export const CommitWrapper = styled.div`
+  width: 100%;
   height: 84px;
   display: flex;
-  flex-direction: column;
-  /* align-items: flex-start;
-  align-self: stretch; */
+  padding: 24px 16px;
+  gap: 24px;
+  align-items: center;
   border-radius: 6px;
   border: 1px solid rgba(208, 215, 222, 0.7);
   background: ${PALETTE.MAIN_WHITE};
 `;
 
-export const CommitUser = styled.div``;
+export const CommitContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
 
-export const Text = styled.p<{ color: string; fontSize: number }>`
+export const CommitMessage = styled.div``;
+
+export const CommitUserInfo = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const Img = styled.img<{ width: number; height: number; radius: number }>`
+  width: ${props => props.width};
+  height: ${props => props.height};
+  border-radius: ${props => props.radius}px;
+  box-shadow: 0px 0px 0px 1px rgba(31, 35, 40, 0.15);
+`;
+
+export const JobBadgeList = styled.div``;
+
+export const Text = styled.span<{ color?: string; fontSize?: number; fontWeight?: number }>`
   color: ${props => props.color};
-  font-size: ${props => props.fontSize || ` font-size: 4`};
+  font-size: ${props => (props.fontSize ? `${props.fontSize}px` : '14px')};
+  font-weight: ${props => (props.fontWeight ? `${props.fontWeight}` : '400')};
 `;
--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@/apis', replacement: '/src/apis/' },
+      { find: '@/stores', replacement: '/src/stores/' },
+      { find: '@/types', replacement: '/src/types/' },
+      { find: '@/pages', replacement: '/src/pages/' },
+      { find: '@/customhooks', replacement: '/src/customhooks/' },
+      { find: '@/styles', replacement: '/src/styles/' },
+      { find: '@/constrants', replacement: '/src/constrants/' },
+      { find: '@/components', replacement: '/src/components/' },
+    ],
+  },
+});
