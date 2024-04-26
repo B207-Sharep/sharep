2e7727d21a4fae65332e0937c5703a7ed5e4a519
김성제 <rlatjdwp0211@naver.com>
2024-04-26 15:24:33 +0900

    [Feat] JobBadge 수정
    - BE 응답값 변경으로 인한 수정

--- fe/src/components/JobBadge/JobBadge.tsx
+++ fe/src/components/JobBadge/JobBadge.tsx
@@ -3,18 +3,18 @@ import * as S from './JobBadgeStyle';
 import * as T from '@types';
 
 const JOB_COLOR_PALETTE: {
-  [key: string]: { BG_COLOR: string; FONT_COLOR: string };
+  [key: string]: { BG_COLOR: string; FONT_COLOR: string; TEXT: string };
 } = {
-  FE: { BG_COLOR: '#FFEFF1', FONT_COLOR: '#FD5B71' },
-  BE: { BG_COLOR: '#E6FCF4', FONT_COLOR: '#07E092' },
-  Infra: { BG_COLOR: '#F2F2F2', FONT_COLOR: '#828282' },
-  Design: { BG_COLOR: '#F5EEFC', FONT_COLOR: '#9B51E0' },
+  FRONT_END: { BG_COLOR: '#FFEFF1', FONT_COLOR: '#FD5B71', TEXT: 'FE' },
+  BACK_END: { BG_COLOR: '#E6FCF4', FONT_COLOR: '#07E092', TEXT: 'BE' },
+  INFRA: { BG_COLOR: '#F2F2F2', FONT_COLOR: '#828282', TEXT: 'Infra' },
+  DESIGNER: { BG_COLOR: '#F5EEFC', FONT_COLOR: '#9B51E0', TEXT: 'Design' },
 };
 
 export default function JobBadge({ job }: T.JobBadgeProps) {
   return (
     <S.Wrapper bgColor={JOB_COLOR_PALETTE[job].BG_COLOR} fontColor={JOB_COLOR_PALETTE[job].FONT_COLOR}>
-      {job}
+      {JOB_COLOR_PALETTE[job].TEXT}
     </S.Wrapper>
   );
 }
