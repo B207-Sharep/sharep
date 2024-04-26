fca6e6fa53c32f0e9ab3ef3bb339a35a6b4a4fb2
임서정 <sjytis14@naver.com>
2024-04-26 15:18:35 +0900

    [Feat] props 변경

--- fe/src/pages/CommitHistory/CommitHistory.tsx
+++ fe/src/pages/CommitHistory/CommitHistory.tsx
@@ -4,8 +4,18 @@ import React from 'react';
 export default function CommitHistory() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', width: '100%', gap: 10 }}>
-      <Commit imageUrl="https://via.placeholder.com/1440x1024" />
-      <Commit />
+      <Commit
+        description="도커란 무엇인가"
+        nickname="임서정"
+        createdAt="2024-04-26"
+        imageUrl="https://via.placeholder.com/1440x1024"
+      />
+      <Commit
+        description="??!!!??"
+        nickname="오상훈"
+        createdAt="2024-04-26"
+        userImageUrl="https://xsgames.co/randomusers/assets/avatars/pixel/1.jpg"
+      />
     </div>
   );
 }
