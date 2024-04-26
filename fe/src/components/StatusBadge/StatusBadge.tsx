004540d72edd4fe36c534efa2144acc10889beaa
김성제 <rlatjdwp0211@naver.com>
2024-04-26 14:35:25 +0900

    [Chore] StatusBadge 수정
    - BE 응답이 변경됨에 따라 props type과 상수 수정

--- fe/src/components/StatusBadge/StatusBadge.tsx
+++ fe/src/components/StatusBadge/StatusBadge.tsx
@@ -3,18 +3,18 @@ import * as S from './StatusBadgeStyle';
 import * as T from '@types';
 
 const STATUS_COLOR_PALETTE: {
-  [key: string]: { BG_COLOR: string; FONT_COLOR: string; DOT_COLOR: string; TEXT: string };
+  [key: string]: { BG_COLOR: string; FONT_COLOR: string; DOT_COLOR: string };
 } = {
-  '1': { BG_COLOR: '#E3E2E0', FONT_COLOR: '#595754', DOT_COLOR: '#91918E', TEXT: '시작 전' }, // 시작 전
-  '2': { BG_COLOR: '#D3E5EF', FONT_COLOR: '#193347', DOT_COLOR: '#5B97BD', TEXT: '진행 중' }, // 진행 중
-  '3': { BG_COLOR: '#E9FFEF', FONT_COLOR: '#409261', DOT_COLOR: '#409261', TEXT: '완료' }, // 완료
+  '시작 전': { BG_COLOR: '#E3E2E0', FONT_COLOR: '#595754', DOT_COLOR: '#91918E' },
+  '진행 중': { BG_COLOR: '#D3E5EF', FONT_COLOR: '#193347', DOT_COLOR: '#5B97BD' },
+  완료: { BG_COLOR: '#E9FFEF', FONT_COLOR: '#409261', DOT_COLOR: '#409261' },
 };
 
 export default function StatusBadge({ status }: T.StatusBadgeProps) {
   return (
     <S.Wrapper bgColor={STATUS_COLOR_PALETTE[status].BG_COLOR} fontColor={STATUS_COLOR_PALETTE[status].FONT_COLOR}>
       <S.Dot bgColor={STATUS_COLOR_PALETTE[status].DOT_COLOR} />
-      {STATUS_COLOR_PALETTE[status].TEXT}
+      {status}
     </S.Wrapper>
   );
 }
