7d679ed3fb8e38929ecdb3690af0fd2611ebd329
조성규 <jo41010@naver.com>
2024-04-26 11:10:08 +0900

    [style] sidebar design without icons

--- fe/src/components/SideBar/SideBar.tsx
+++ fe/src/components/SideBar/SideBar.tsx
@@ -6,10 +6,77 @@ export default function SideBar() {
     <>
       <S.SideBarWrapper>
         <S.SideBarProfile>
-          <S.SideBarProfilePhoto>thisis</S.SideBarProfilePhoto>
-          <S.SideBarProfilePhoto>thisis</S.SideBarProfilePhoto>
+          <S.SideBarProfilePhoto></S.SideBarProfilePhoto>
+          <S.SideBarProfileName>
+            <S.SideBarFont size="24px" weight="bold">
+              김성제
+            </S.SideBarFont>
+            <S.SideBarFont size="16px" weight="400">
+              @ssafy
+            </S.SideBarFont>
+          </S.SideBarProfileName>
         </S.SideBarProfile>
-        <S.SideBarProfile>NIVE</S.SideBarProfile>
+        <S.SideBarNavList>
+          <S.SideBarNavMain>
+            {/* 내프로젝트영역 */}
+            <S.SideBarMyProject>
+              <S.SideBarTitle>
+                <S.SideBarFont size="18px" weight="bold">
+                  Share.P
+                </S.SideBarFont>
+                <div>
+                  <button>버튼1</button>
+                  <button>버튼2</button>
+                </div>
+              </S.SideBarTitle>
+              <S.SideBarContents>
+                <S.SideBarFont size="14px" weight="200">
+                  팀 대시보드
+                </S.SideBarFont>
+              </S.SideBarContents>
+              <S.SideBarContents>
+                <S.SideBarFont size="14px" weight="200">
+                  나의 대시보드
+                </S.SideBarFont>
+              </S.SideBarContents>
+            </S.SideBarMyProject>
+            {/* 내프로젝트끝 */}
+            {/* 명세서영역시작 */}
+
+            <S.SideBarMyProject>
+              <S.SideBarTitle>
+                <S.SideBarFont size="18px" weight="bold">
+                  주요 명세서
+                </S.SideBarFont>
+              </S.SideBarTitle>
+              <S.SideBarContents>
+                <S.SideBarFont size="14px" weight="200">
+                  기능 명세서
+                </S.SideBarFont>
+              </S.SideBarContents>
+              <S.SideBarContents>
+                <S.SideBarFont size="14px" weight="200">
+                  API 명세서
+                </S.SideBarFont>
+              </S.SideBarContents>
+              <S.SideBarContents>
+                <S.SideBarFont size="14px" weight="200">
+                  화면 정의서
+                </S.SideBarFont>
+              </S.SideBarContents>
+              <S.SideBarContents>
+                <S.SideBarFont size="14px" weight="200">
+                  인프라 명세서
+                </S.SideBarFont>
+              </S.SideBarContents>
+            </S.SideBarMyProject>
+          </S.SideBarNavMain>
+          <S.SideBarContents>
+            <S.SideBarFont size="12px" weight="200">
+              알림
+            </S.SideBarFont>
+          </S.SideBarContents>
+        </S.SideBarNavList>
       </S.SideBarWrapper>
     </>
   );
--- fe/src/components/SideBar/SideBarStyle.ts
+++ fe/src/components/SideBar/SideBarStyle.ts
@@ -1,25 +1,70 @@
 import styled from 'styled-components';
 
+interface FontOption {
+  size: string;
+  weight: string;
+}
+
 export const SideBarWrapper = styled.div`
   width: 260px;
   height: 100vh;
-  background-color: green;
+  padding: 16px 0px;
+  row-gap: 24px;
+  /* background-color: green; */
   display: flex;
   flex-direction: column;
 `;
 
 export const SideBarProfile = styled.div`
-  width: 100%;
-  height: 100px;
-  background-color: blue;
+  column-gap: 14px;
+  padding: 10px 10px;
   display: flex;
   flex-direction: row;
 `;
 
 export const SideBarProfilePhoto = styled.div`
-  width: 50%;
-  height: 100%;
-  background-color: red;
+  width: 72px;
+  height: 72px;
   display: flex;
   flex-direction: row;
+  background-color: red;
+`;
+
+export const SideBarProfileName = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+export const SideBarNavList = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-between;
+`;
+
+export const SideBarNavMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 24px;
+`;
+
+export const SideBarMyProject = styled(SideBarNavMain)`
+  row-gap: 10px;
+`;
+
+export const SideBarContents = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 12px 12px;
+  column-gap: 10px;
+`;
+
+export const SideBarTitle = styled(SideBarContents)`
+  justify-content: space-between;
+  border-bottom: solid 1px black;
+`;
+
+export const SideBarFont = styled.div<FontOption>`
+  font-size: ${props => props.size};
+  font-weight: ${props => props.weight};
 `;
