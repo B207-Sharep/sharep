717ebfb153fea48d2b67e8dd078d2361eaad2f21
유재건 <yjk9805@naver.com>
2024-04-28 21:56:31 +0900

    [Feat] Project Entity @EntityGraph 추가 & QueryDSLRepository

--- be/src/main/java/com/sharep/be/modules/project/ProjectRepository.java
+++ be/src/main/java/com/sharep/be/modules/project/ProjectRepository.java
@@ -1,7 +1,14 @@
 package com.sharep.be.modules.project;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+import java.util.Optional;
 
+public interface ProjectRepository extends JpaRepository<Project, Long>, ProjectRepositoryCustom{
+    @EntityGraph(attributePaths = "leader")
+    Optional<Project> findWithLeaderById(Long projectId);
+
+    @EntityGraph(attributePaths = {"leader", "members"})
+    Optional<Project> findWithLeaderAndMembersById(Long projectId);
 }
