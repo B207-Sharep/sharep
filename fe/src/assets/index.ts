944ddc35635526c218f98f093be805c309dc9bf0
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-04-28 14:30:01 +0900

    [Refactor] assets re-export
    - 프로젝트 내부 src/assets에 들어가 있는 icon들 re-export

---
+++ fe/src/assets/index.ts
@@ -0,0 +1,10 @@
+import ApiDocs from './svgs/api-docs-icon.svg';
+import EtcDocs from './svgs/etc-docs-icon.svg';
+import InfraDocs from './svgs/infra-docs-icon.svg';
+import MyDashboard from './svgs/my-dashboard-icon.svg';
+import Alarm from './svgs/noti.svg';
+import PlanDocs from './svgs/plan-docs-icon.svg';
+import ScreenDefinition from './svgs/screen -definition-icon.svg';
+import TeamDashboard from './svgs/team-dashboard-icon.svg';
+
+export { ApiDocs, EtcDocs, InfraDocs, MyDashboard, Alarm, PlanDocs, ScreenDefinition, TeamDashboard };
