466832404ca71eb706a480d7a902ad9e7abbe7f2
김성제 <rlatjdwp0211@naver.com>
2024-04-30 10:25:10 +0900

    [Feat] JobBadge 수정
    - JobBadge 선택 가능하게 수정

--- fe/src/components/JobBadge/JobBadgeStyle.ts
+++ fe/src/components/JobBadge/JobBadgeStyle.ts
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div<{ $bgColor: string; $fontColor: string }>`
+export const Wrapper = styled.div<{
+  $bgColor: string;
+  $fontColor: string;
+  $selected: false | { state: boolean; onClick: (...args: any) => void };
+}>`
   width: fit-content;
   height: fit-content;
   padding: 4px 6px;
   border-radius: 6px;
   font-size: 12px;
-  color: ${({ $fontColor }) => $fontColor};
-  background-color: ${({ $bgColor }) => $bgColor};
+  color: ${({ $fontColor, $selected }) => ($selected && $selected.state ? 'white' : $fontColor)};
+  background-color: ${({ $bgColor, $fontColor, $selected }) => ($selected && $selected.state ? $fontColor : $bgColor)};
+  cursor: ${({ $selected }) => $selected && 'pointer'};
 `;
