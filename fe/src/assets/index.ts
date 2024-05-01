7e17d3135dee9ae12e5137ee9d0160f8005105d5
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-01 16:59:25 +0900

    [Add] Icon 추가
    - Teamdashboard에서 사용할 Icon 추가
    - Re-export 및 기존 화면 정의서 icon 오타 수정

--- fe/src/assets/index.ts
+++ fe/src/assets/index.ts
@@ -4,11 +4,15 @@ import InfraDocs from './svgs/infra-docs-icon.svg?react';
 import MyDashboard from './svgs/my-dashboard-icon.svg?react';
 import Alarm from './svgs/noti.svg?react';
 import PlanDocs from './svgs/plan-docs-icon.svg?react';
-import ScreenDefinition from './svgs/screen -definition-icon.svg?react';
+import ScreenDefinition from './svgs/screen-definition-icon.svg?react';
 import TeamDashboard from './svgs/team-dashboard-icon.svg?react';
 import CurrentStateTitle from './svgs/current-state-title.svg?react';
 import MainTitle from './svgs/main-title-icon.svg?react';
 import TextContentTitle from './svgs/text-content-title.svg?react';
+import CurrentWork from './svgs/current-work-icon.svg?react';
+import Gallery from './svgs/gallery-icon.svg?react';
+import GantChart from './svgs/gant-chart-icon.svg?react';
+import YesterdayWork from './svgs/yesterday-work-icon.svg?react';
 
 export {
   ApiDocs,
@@ -22,4 +26,8 @@ export {
   CurrentStateTitle,
   MainTitle,
   TextContentTitle,
+  CurrentWork,
+  Gallery,
+  GantChart,
+  YesterdayWork,
 };
