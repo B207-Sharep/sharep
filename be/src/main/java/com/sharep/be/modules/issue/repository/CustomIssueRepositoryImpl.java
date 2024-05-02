5ad5f80c9e27853dfdee512d0ce0f3002f94c5f0
오상훈 <ph38230119@gmail.com>
2024-05-02 23:23:50 +0900

    [Refactor] 필요없는 DTO 삭제

--- be/src/main/java/com/sharep/be/modules/issue/repository/CustomIssueRepositoryImpl.java
+++ be/src/main/java/com/sharep/be/modules/issue/repository/CustomIssueRepositoryImpl.java
@@ -1,15 +1,16 @@
 package com.sharep.be.modules.issue.repository;
 
+import static com.sharep.be.modules.account.QAccount.account;
+import static com.sharep.be.modules.api.QApi.api;
+import static com.sharep.be.modules.assignee.QAssignee.assignee;
 import static com.sharep.be.modules.issue.QIssue.issue;
-import static com.sharep.be.modules.storyboard.QStoryboard.storyboard;
+import static com.sharep.be.modules.job.QJob.job;
+import static com.sharep.be.modules.member.QMember.member;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sharep.be.modules.issue.Issue;
-import com.sharep.be.modules.issue.QIssue;
-import com.sharep.be.modules.issue.dto.FeatureDto;
 import com.sharep.be.modules.issue.type.IssueType;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -20,57 +21,12 @@ public class CustomIssueRepositoryImpl implements CustomIssueRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Optional<List<FeatureDto>> findFeatureIssuesByProjectIdAndIssueType(Long projectId,
-            IssueType issueType) {
-
-        List<Issue> issues = queryFactory
-                .select(issue)
-                .from(issue)
-                .leftJoin(issue.storyboards, storyboard).fetchJoin()
-                .leftJoin(storyboard.screenIssue, new QIssue("screenIssue")).fetchJoin()
-                .leftJoin(issue.assignees).fetchJoin()
-                .where(issue.type.eq(IssueType.FEATURE)
-                        .and(issue.project.id.eq(projectId)))
-                .fetch();
-
-        List<FeatureDto> features = null;
-
-//        List<FeatureDto> features = queryFactory.select(
-//                        Projections.fields(FeatureDto.class, issue.id, issue.epic, issue.issueName,
-//                                issue.priority)).from(issue)
-//                .where(issue.type.eq(IssueType.FEATURE).and(issue.projectId.eq(projectId))).fetch();
-//
-//        features.forEach(featureDto -> {
-//            List<ScreenResponseDto> screenIssues = queryFactory.select(
-//                            Projections.constructor(ScreenResponseDto.class, storyboard.screenIssue.id,
-//                                    storyboard.screenIssue.issueName)).from(storyboard)
-//                    .leftJoin(storyboard.screenIssue)
-//                    .where(storyboard.feature_issue_id.eq(featureDto.getId())).fetch();
-//
-//            featureDto.setScreenIssues(screenIssues);
-//
-//            List<AssignedMemberResponseDto> assignees = queryFactory.select(
-//                            Projections.constructor(AssignedMemberResponseDto.class, assignee.member.id,
-//                                    assignee.state, assignee.startedAt, assignee.finishedAt)).from(assignee)
-//                    .where(assignee.issue.id.eq(featureDto.getId())).fetch();
-//
-//            featureDto.setAssignees(assignees);
-//
-//            LocalDateTime startedTime = assignees.stream()
-//                    .map(AssignedMemberResponseDto::getStartedAt)
-//                    .min(LocalDateTime::compareTo)
-//                    .orElse(null);
-//
-//            LocalDateTime finishedTime = assignees.stream()
-//                    .map(AssignedMemberResponseDto::getFinishedAt)
-//                    .max(LocalDateTime::compareTo)
-//                    .orElse(null);
-//
-//            featureDto.setStartedTime(startedTime);
-//            featureDto.setFinishedTime(finishedTime);
-//        });
-
-        return Optional.of(features);
+    public List<Issue> findIssuesByMemberIdAndIssueType(Long memberId, IssueType issueType) {
+        return queryFactory.select(issue).from(issue).leftJoin(issue.api, api).fetchJoin()
+                .leftJoin(issue.jobs, job).fetchJoin().innerJoin(issue.assignees, assignee)
+                .fetchJoin().innerJoin(assignee.member, member).fetchJoin()
+                .innerJoin(member.account, account).where(issue.type.eq(issueType))
+                .where(member.id.eq(memberId)).fetch();
     }
 }
 
