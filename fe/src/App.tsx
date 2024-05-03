21df7499abacb2fad404167dd5a4f85a1df35084
김성제 <rlatjdwp0211@naver.com>
2024-05-03 10:43:06 +0900

    [Feat] Main page
    - 라우팅 버튼 생성

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<div>main</div>} />
+        <Route path="/" element={<Page.Main />} />
         <Route path="/seo-jeong" element={<Page.SeoJeong />} />
         <Route path="/sung-gu" element={<Page.SungGu />} />
         <Route path="/sung-je" element={<Page.SungJe />} />
