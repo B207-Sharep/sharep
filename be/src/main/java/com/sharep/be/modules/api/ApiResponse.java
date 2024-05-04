e067ab8f2694013b04736214e93e5699005472bb
오상훈 <ph38230119@gmail.com>
2024-05-04 16:29:27 +0900

    [Refactor] Dirty Checking 반영
    Api, Assignee, Job Response NPE 수정
    Assert 메서드 static import
    deleteApi 로직을 Dirty Checking을 이용하도록 수정

--- be/src/main/java/com/sharep/be/modules/api/ApiResponse.java
+++ be/src/main/java/com/sharep/be/modules/api/ApiResponse.java
@@ -1,19 +1,22 @@
 package com.sharep.be.modules.api;
 
 import com.sharep.be.modules.api.type.MethodType;
+import java.util.Optional;
 import lombok.Builder;
 
 @Builder
 public record ApiResponse(Long id, String request, String response, String url, MethodType method) {
 
     public static ApiResponse from(Api api) {
-        return ApiResponse.builder()
-                .id(api.getId())
-                .request(api.getRequest())
-                .response(api.getResponse())
-                .url(api.getUrl())
-                .method(api.getMethod())
-                .build();
+        return Optional.of(
+                ApiResponse.builder()
+                        .id(api.getId())
+                        .request(api.getRequest())
+                        .response(api.getResponse())
+                        .url(api.getUrl())
+                        .method(api.getMethod())
+                        .build()
+        ).orElse(null);
     }
 
     @Builder
