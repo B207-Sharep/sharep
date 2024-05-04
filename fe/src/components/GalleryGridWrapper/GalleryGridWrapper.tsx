42de201930ef42d2673a2c346bf92a5725fcd037
seojeong <sjytis14@naver.com>
2024-05-05 01:04:14 +0900

    [Feat] 화면정의서 페이지 새로 만들기 버튼 추가

--- fe/src/components/GalleryGridWrapper/GalleryGridWrapper.tsx
+++ fe/src/components/GalleryGridWrapper/GalleryGridWrapper.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import * as S from './GalleryGridWrapperStyle';
 import GalleryCard from '../GalleryCard/GalleryCard';
 import { GalleryGridWrapperProps } from '@/types/components/GalleryGridWrapper';
+import { PALETTE } from '@/styles';
+import { Plus } from 'lucide-react';
 
 export default function GalleryGridWrapper({ issueList }: GalleryGridWrapperProps) {
+  const handleAddBtn = () => {
+    console.log('add');
+  };
+
   return (
     <S.Grid>
       <S.CardList>
@@ -16,6 +22,14 @@ export default function GalleryGridWrapper({ issueList }: GalleryGridWrapperProp
             imageUrl={issue.issueType === 'SCREEN' ? `https://picsum.photos/id/${index}/300/200` : undefined}
           />
         ))}
+        <S.CardAddBtn className="hover-moving" onClick={handleAddBtn}>
+          <S.TextContainer>
+            <Plus size={16} />
+            <S.StyledText color={PALETTE.LIGHT_BLACK} fontSize={16}>
+              새로 만들기
+            </S.StyledText>
+          </S.TextContainer>
+        </S.CardAddBtn>
       </S.CardList>
     </S.Grid>
   );
