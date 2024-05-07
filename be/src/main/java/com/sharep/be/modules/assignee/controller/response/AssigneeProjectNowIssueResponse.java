4a2de855b1dd79775a404cce844a524b36d9e60d
Seungmin Lee <mehot98@gmail.com>
2024-05-07 19:46:33 +0900

    [Fix] 팀원 현재 이슈 반환값 변경

--- be/src/main/java/com/sharep/be/modules/assignee/controller/response/AssigneeProjectNowIssueResponse.java
+++ be/src/main/java/com/sharep/be/modules/assignee/controller/response/AssigneeProjectNowIssueResponse.java
@@ -1,7 +1,5 @@
 package com.sharep.be.modules.assignee.controller.response;
 
-import com.sharep.be.modules.account.Account;
-import com.sharep.be.modules.account.dto.AccountDto;
 import com.sharep.be.modules.issue.Issue;
 import com.sharep.be.modules.issue.IssueNowResponse;
 import com.sharep.be.modules.member.Member;
