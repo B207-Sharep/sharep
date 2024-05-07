e2f0b7105a4964637cd30b0689f46cea35427873
이승민 <mehot98@gmail.com>
2024-05-07 12:11:07 +0900

    [Feat] 기여도 반환값 수정

--- be/src/main/java/com/sharep/be/modules/job/controller/response/JobContributionResponse.java
+++ be/src/main/java/com/sharep/be/modules/job/controller/response/JobContributionResponse.java
@@ -1,6 +1,8 @@
 package com.sharep.be.modules.job.controller.response;
 
 import com.sharep.be.modules.job.domain.Job;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -11,9 +13,20 @@ public record JobContributionResponse() {
 
         SortedMap<String, Integer> response = new TreeMap<>();
 
+        LocalDate todayLocalDate = LocalDateTime.now().toLocalDate();
+
+        for(int i = 0; i < 30; i++){
+            String todayString = todayLocalDate.minusDays(i).toString();
+
+            response.put(todayString, 0);
+        }
+
         jobs.forEach(job -> {
-                    String date = String.valueOf(job.getCreatedAt().toLocalDate());
-                    Integer now = response.getOrDefault(date, 0);
+                    String date = job.getCreatedAt().toLocalDate().toString();
+
+                    if(!response.containsKey(date)) return;
+
+                    Integer now = response.get(date);
                     response.put(date, now + 1);
                 }
         );
