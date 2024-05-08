ced18e4696a588993476d5524ff524dd338d17af
오상훈 <ph38230119@gmail.com>
2024-05-08 10:01:48 +0900

    [Feat] 이슈 담당자 정보 추가
    칸반 이슈를 담당자의 상태로 렌더하여 담당자가 상태를 변경할 때 마다 칸반 카드의 상태가 변경되도록 수정

--- be/src/main/java/com/sharep/be/modules/assignee/controller/response/AssigneeResponse.java
+++ be/src/main/java/com/sharep/be/modules/assignee/controller/response/AssigneeResponse.java
@@ -1,19 +1,22 @@
 package com.sharep.be.modules.assignee.controller.response;
 
 import com.sharep.be.modules.assignee.domain.Assignee;
+import com.sharep.be.modules.assignee.domain.State;
 import java.util.Optional;
 import lombok.Builder;
 
 @Builder
-public record AssigneeResponse(String name, String imageUrl) {
+public record AssigneeResponse(Long id, State state, Long accountId, String name, String imageUrl) {
 
     public static AssigneeResponse from(Assignee assignee) {
         return Optional.ofNullable(assignee)
                 .map(a -> AssigneeResponse.builder()
+                        .id(assignee.getId())
+                        .state(assignee.getState())
+                        .accountId(assignee.getMember().getAccount().getId())
                         .name(a.getMember().getAccount().getNickname())
                         .imageUrl(a.getMember().getAccount().getImageUrl())
                         .build()
                 ).orElse(null);
     }
-
 }
