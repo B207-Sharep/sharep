b5d09d15ae678316040837033994ab66e76bbecd
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-09 22:44:40 +0900

    [Remove] TextCel -> SelectAssigneesCel
    - Todo : member list 받아 온 후 SelectAssigneesCel에 반영

--- fe/src/components/ManualTable/Subs/TextAreaCel/TextAreaCelStyle.ts
+++ fe/src/components/ManualTable/Subs/TextAreaCel/TextAreaCelStyle.ts
@@ -5,7 +5,7 @@ export const TextAreaCel = styled.textarea<{ $fixedWidth: string; $isEditingMode
   max-width: 100%;
   width: ${({ $fixedWidth }) => $fixedWidth};
   min-height: 100%;
-  min-width: fit-content;
+  min-width: ${({ $fixedWidth }) => $fixedWidth};
   border-radius: ${({ $isEditingMode }) => $isEditingMode && '6px'};
   border: ${({ $isEditingMode }) => ($isEditingMode ? `1px solid ${PALETTE.MAIN_BACKGROUND}` : 'none')};
   box-shadow: ${({ $isEditingMode }) => $isEditingMode && '2px 2px 16px rgba(0, 0, 0, 0.36)'};
@@ -13,6 +13,8 @@ export const TextAreaCel = styled.textarea<{ $fixedWidth: string; $isEditingMode
   resize: none;
   outline: none;
   overflow: hidden;
+  text-align: left;
+  padding: 8px 16px;
 
   &:focus {
     border: none;
