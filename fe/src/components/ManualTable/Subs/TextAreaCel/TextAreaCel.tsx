0133da3163d6073d067a73f5d80a59855a7b0d92
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-05-09 09:07:43 +0900

    [Feat] Textarea Cel
    - 입력 시 height 자동 조절

--- fe/src/components/ManualTable/Subs/TextAreaCel/TextAreaCel.tsx
+++ fe/src/components/ManualTable/Subs/TextAreaCel/TextAreaCel.tsx
@@ -3,39 +3,53 @@ import * as S from './TextAreaCelStyle';
 import * as T from '@types';
 
 export default function TextAreaCel({ initialState, fixedWidth }: T.CelProps) {
-  const celRef = useRef<HTMLTextAreaElement>(null);
+  const textContent = useRef<HTMLSpanElement>(null);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
   const [value, setValue] = useState(initialState || '');
   const [isEditingMode, setIsEditingMode] = useState(false);
   const [isPressingShiftKey, setIsPressingShiftKey] = useState(false);
 
   useEffect(() => {
-    if (isEditingMode && celRef.current) celRef.current.focus();
+    if (isEditingMode && textareaRef.current) textareaRef.current.focus();
   }, [isEditingMode]);
 
   const handleCelClick = (toggledValue: boolean) => {
-    if (toggledValue) celRef.current?.focus();
-    else celRef.current?.blur();
+    if (toggledValue) textareaRef.current?.focus();
+    else textareaRef.current?.blur();
 
     setIsEditingMode(() => toggledValue);
   };
 
   const handleOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setValue(e.target.value);
+    setValue(e.target.value || '');
+
+    if (textareaRef.current) {
+      const { scrollHeight, style } = textareaRef.current;
+      style.height = `${scrollHeight}px`;
+    }
   };
 
   const handleKeyboardEventOnEditor = (e: React.KeyboardEvent<HTMLTextAreaElement>, toggledValue: boolean) => {
-    console.log(`test :`, e.keyCode);
-    if ((!isPressingShiftKey && e.keyCode === 13) || e.keyCode === 27) handleCelClick(false);
-    if (e.keyCode === 16) setIsPressingShiftKey(() => toggledValue);
+    if (textareaRef.current) {
+      const { scrollHeight, style } = textareaRef.current;
+      const curHeight = Number(style.height.replace('px', ''));
+
+      if (e.key === 'Shift') setIsPressingShiftKey(() => toggledValue);
+
+      if ((!isPressingShiftKey && e.key === 'Enter') || e.key === 'Escape') handleCelClick(false);
+
+      if (e.key === 'Backspace' && toggledValue && curHeight > 36) style.height = `${scrollHeight - 16}px`;
+    }
   };
 
   return (
     <S.TextAreaCel
-      ref={celRef}
+      ref={textareaRef}
       value={value}
       onChange={handleOnChange}
       onKeyDown={e => handleKeyboardEventOnEditor(e, true)}
       onKeyUp={e => handleKeyboardEventOnEditor(e, false)}
+      onFocus={() => handleCelClick(true)}
       onBlur={() => handleCelClick(false)}
       $fixedWidth={fixedWidth}
       $isEditingMode={isEditingMode}
--- fe/src/components/ManualTable/Subs/TextAreaCel/TextAreaCelStyle.ts
+++ fe/src/components/ManualTable/Subs/TextAreaCel/TextAreaCelStyle.ts
@@ -4,9 +4,17 @@ import { PALETTE } from '@/styles';
 export const TextAreaCel = styled.textarea<{ $fixedWidth: string; $isEditingMode: boolean }>`
   max-width: 100%;
   width: ${({ $fixedWidth }) => $fixedWidth};
+  min-height: 100%;
   min-width: fit-content;
-  min-height: fit-content;
   border-radius: ${({ $isEditingMode }) => $isEditingMode && '6px'};
+  border: ${({ $isEditingMode }) => ($isEditingMode ? `1px solid ${PALETTE.MAIN_BACKGROUND}` : 'none')};
   box-shadow: ${({ $isEditingMode }) => $isEditingMode && '2px 2px 16px rgba(0, 0, 0, 0.36)'};
   color: ${PALETTE.TABLE_CONTENT};
+  resize: none;
+  outline: none;
+  overflow: hidden;
+
+  &:focus {
+    border: none;
+  }
 `;
